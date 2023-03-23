489a01ef2b33ec09405933e2f8bacc5ce304564c
eduardonk9999 <thiago.novak7@gmail.com>
2023-03-23 13:55:36 -0300

    metodos e atributos da caneta

--- AulasOO/src/Caneta.java
+++ AulasOO/src/Caneta.java
@@ -5,15 +5,27 @@ public class Caneta {
     int carga;
     boolean tampada;
 
-    void rabiscar() {
+    void status() {
+        System.out.print("Modelo: " + this.modelo);
+        System.out.print("Uma caneta " + this.cor);
+        System.out.print("Ponta: " + this.ponta);
+        System.out.print("Carga: " + this.carga);
+        System.out.print(" está tampada: " + this.tampada);
+    }
 
+    void rabiscar() {
+        if(this.tampada == true) {
+            System.out.println("ERRO! Não posso rabiscar");
+        } else {
+            System.out.println("Estou Rabiscando");
+        }
     }
 
     void tampar() {
-
+        this.tampada = true;
     }
 
     void destampar() {
-
+        this.tampada = false;
     }
 }
